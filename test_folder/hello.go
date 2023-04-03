e47d8ccb40fcb7cc54ed94ca581f4ace90e83926
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-04-03 01:46:09 -0400

    adding a debounce to prevent unwanted file changes with go run

--- test_folder/hello.go
+++ test_folder/hello.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	println("debug: hello this")
+	println("debug: hello")
 	fmt.Println("vim-go")
 }
--- watcher.go
+++ watcher.go
@@ -8,9 +8,29 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"sync"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+var debounceDuration = 5000 * time.Millisecond
+
+func debounce(execution func(), duration time.Duration) func() {
+	var timer *time.Timer
+	var mu sync.Mutex
+
+	return func() {
+		mu.Lock()
+		defer mu.Unlock()
+
+		if timer != nil {
+			timer.Stop()
+		}
+
+		timer = time.AfterFunc(duration, execution)
+	}
+}
 
 func main() {
 	dirFlag := flag.String("d", ".", "Directory to watch for changes")
@@ -39,33 +59,37 @@ func main() {
 
 	fmt.Printf("Watching directory: %s\n", dirToWatch)
 
+	executeCommand := func() {
+		if commandToRun != "" {
+			var cmd *exec.Cmd
+			if runtime.GOOS == "windows" {
+				cmd = exec.Command("powershell.exe", "-Command", commandToRun)
+			} else {
+				cmd = exec.Command("sh", "-c", commandToRun)
+			}
+			cmd.Dir = dirToWatch
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err := cmd.Run()
+			if err != nil {
+				log.Printf("Command execution failed: %s\n", err)
+			} else {
+				log.Printf("Command executed successfully\n")
+			}
+		}
+	}
+
+	debouncedExecuteCommand := debounce(executeCommand, debounceDuration)
+
 	// Process file system events
 	for {
 		select {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
 				relativePath, _ := filepath.Rel(dirToWatch, event.Name)
-
-				if commandToRun != "" {
-					var cmd *exec.Cmd
-					if runtime.GOOS == "windows" {
-						cmd = exec.Command("powershell.exe", "-Command", commandToRun)
-					} else {
-						cmd = exec.Command("sh", "-c", commandToRun)
-					}
-					cmd.Dir = dirToWatch
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-
-					err := cmd.Run()
-					if err != nil {
-						log.Printf("Command execution failed: %s\n", err)
-					} else {
-						log.Printf("Command executed successfully\n")
-					}
-				} else {
-					log.Printf("File changed: %s\n", relativePath)
-				}
+				log.Printf("File changed: %s\n", relativePath)
+				debouncedExecuteCommand()
 			}
 		case err := <-watcher.Errors:
 			log.Printf("Watcher error: %s\n", err)
